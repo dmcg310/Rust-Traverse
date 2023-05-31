cb53087fcfe5214e68571ea2c0979293acae6e39
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-05-31 18:40:49 +0100

    feat: basic tui setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{
+    backend::CrosstermBackend,
+    widgets::{Block, Borders},
+    Terminal,
+};
+use std::{io, thread, time::Duration};
+
+fn main() -> Result<(), io::Error> {
+    let files = ["foo.txt", "bar.rs", "baz.js"];
+    let dirs = ["foo", "bar", "baz"];
+
+    enable_raw_mode()?;
+    let stdout = io::stdout();
+
+    execute!(io::stdout(), EnterAlternateScreen, EnableMouseCapture,)?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().title("Block").borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    thread::spawn(move || loop {
+        thread::sleep(Duration::from_millis(500));
+    });
+
+    loop {
+        if event::poll(Duration::from_millis(500))? {
+            if let event::Event::Key(key) = event::read()? {
+                match key.code {
+                    event::KeyCode::Char('q') => {
+                        break;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture,
+    )?;
+    terminal.show_cursor()?;
+    Ok(())
 }
